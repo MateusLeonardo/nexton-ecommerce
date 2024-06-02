701a8c21d04c9b7cf47260060811a9ed38e56980
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2024-06-02 08:36:16 -0300

    feat: adding logo

---
+++ src/components/logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <div>
+      <h1 className="text-xl font-semibold">NEXTON</h1>
+      <p className="text-xs text-end">ecommerce</p>
+    </div>
+  );
+}
